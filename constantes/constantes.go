0a805d7dc6fa19de747c7f6a280aa44c77c1fb07
claudio-code <suporte2424@outlook.com>
2020-09-01 01:32:32 -0300

    aprendendo sobre tipos

--- constantes/constantes.go
+++ constantes/constantes.go
@@ -1,4 +1,4 @@
-package constantes
+package main
 
 import (
 	"fmt"
@@ -12,4 +12,26 @@ func main() {
 	// forma reduzida de declarar uma variavel
 	area := PI * math.Pow(raio, 2)
 	fmt.Println(area)
+
+	const (
+		a = 2
+		b = 1
+	)
+	fmt.Print(a)
+	fmt.Print(b)
+	fmt.Print("\n")
+
+	var (
+		c = 12
+		va = 21
+	)
+	fmt.Print(c)
+	fmt.Print(va)
+	fmt.Print("\n")
+	var e, f bool = true, false
+	fmt.Print(e, f)
+	fmt.Print("\n")
+
+	g, h, i := 2, "cagada", false
+	fmt.Print(g, h, i)
 }
