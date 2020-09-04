2ded4f0cbdb0cdf52569c6c178f52203ff665406
claudio-code <suporte2424@outlook.com>
2020-09-03 23:29:29 -0300

    estudando tipos

--- Tipos/tipos.go
+++ Tipos/tipos.go
@@ -1,1 +1,22 @@
-package Tipos
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+func main() {
+	// numeros inteiros
+	fmt.Println(2, 21212)
+	fmt.Println("tipo =>", reflect.TypeOf(2121))
+
+	// sem sinal (só positivos) uinit8 uinit16 uinit32 uinit64
+	var bite byte = 244
+	fmt.Println("tipo byte é = ", reflect.TypeOf(bite))
+
+	// com sinal uinit8 uinit16 uinit32 uinit64
+	i1 := math.MaxInt64
+	fmt.Println("o valor é = ", i1)
+	fmt.Println("o tipo", reflect.TypeOf(i1))
+}
